988ad7a894937d063e11db566f664c095d4f6759
Leland <63216358+lelanddurley@users.noreply.github.com>
2023-03-19 23:28:55 -0500

    deploy

---
+++ pages/Genealogy.jsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import React from "react";
+import GeneologyProjectImg from "../public/assets/projects/GeneologyProjectPhoto.jpg";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+
+const Genealogy = () => {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[50vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={GeneologyProjectImg}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Genealogy Project</h2>
+          <h3>JavaScript / PostgreSQL / AWS</h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            As a freelancer, I built this application in JavaScript to create a
+            hybrid between a genealogy and a social media platform. Alongside a
+            creating RESTful API this app features user authentication with
+            Google Authentication & Amazon Web Services' Geo-redundant cloud
+            storage databases. This genealogy platform is an impressive CRUD
+            application, which gives users the power to post, interact with, and
+            redefine their family history. This project features client-side &
+            server-side rendering, personalized advertising based on location,
+            and customizeable privacy settings all behind end-to-end Encryption.
+          </p>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> AWS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Next.JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Apache Spark
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Google Auth
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Express.JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> PostgreSQL
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Genealogy;
