8f3a799114112bd96fe1ae79ae59f6367f9058c8
Leland <63216358+lelanddurley@users.noreply.github.com>
2023-03-22 20:51:08 -0500

    Add files via upload

---
+++ components/Skills.jsx
@@ -0,0 +1,107 @@
+import Image from "next/image";
+import React from "react";
+import Html from "../public/assets/skills/html.png";
+import Tailwind from "../public/assets/skills/TailwindLogo.svg";
+import Javascript from "../public/assets/skills/javascript.png";
+import ReactImg from "../public/assets/skills/react.png";
+import CImg from "../public/assets/skills/c--4.svg";
+import PSQLImg from "../public/assets/skills/PSQLlogo.svg";
+import GitImg from "../public/assets/skills/git-icon.svg";
+import NextJS from "../public/assets/skills/nextjs.png";
+
+const Skills = () => {
+  return (
+    <div id="skills" className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#509beb]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="flex inset-y-auto inset-x-auto m-auto">
+                <Image src={Html} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center inset-x-auto inset-y-auto">
+                <h3>HTML</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="flex inset-y-auto inset-x-auto m-auto">
+                <Image src={Tailwind} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center inset-x-auto inset-y-auto">
+                <h3>Tailwind</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="flex inset-y-auto inset-x-auto m-auto">
+                <Image src={Javascript} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center inset-x-auto inset-y-auto">
+                <h3>JavaScript</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="flex inset-y-auto inset-x-auto m-auto">
+                <Image src={ReactImg} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center inset-x-auto inset-y-auto">
+                <h3>React</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="flex inset-y-auto inset-x-auto m-auto">
+                <Image src={CImg} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center inset-x-auto inset-y-auto">
+                <h3>C#</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="flex inset-y-auto inset-x-auto m-auto">
+                <Image src={PSQLImg} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center inset-x-auto inset-y-auto">
+                <h3>PostgreSQL</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="flex inset-y-auto inset-x-auto m-auto">
+                <Image src={GitImg} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center inset-x-auto inset-y-auto">
+                <h3>Git</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="flex inset-y-auto inset-x-auto m-auto">
+                <Image src={NextJS} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center inset-x-auto inset-y-auto">
+                <h3>Next</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
---
+++ pages/Pixie.jsx
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import React from "react";
+import PixieImg from "../public/assets/projects/PixieImg.png";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+
+const Pixie = () => {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[50vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={PixieImg}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Pixie</h2>
+          <h3>Next.JS / PostgreSQL / AWS</h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            I built this app to demonstrate knowledge of working in Next.JS and
+            accessing API endpoints for an enterprise-level venture. The goal of
+            this application is to fight the spread of false media and biased
+            news propagators. Pixie functions as an E-commerce store, social
+            media, and weather platform, but the contributions made by our team
+            have maximized the power of the users. This application features
+            desktop, IOS, and Android support for user privacy (via end-to-end
+            encryption) and real-time weather information. Journalism, history,
+            trends, and weather all come together on this customizable
+            application thanks to our version control system (Git) and
+            Geo-redundant hosting servers.
+          </p>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1 ">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> JavaScript
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Express.JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Next.JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwind
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> OpenStreetMap API
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> PostgreSQL
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Google Auth
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Pixie;
